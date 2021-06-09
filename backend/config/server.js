3a4a052a157da2f693f5d8341ddef8a7fef5ee80
MMShep97 <MMShep97@gmail.com>
2021-06-08 23:49:22 -0500

    feat: add frontend & backend skeleton
    This includes nuxt, apollo, uikit. This commit also sneaks in article and category content type creations for the strapi backend cms

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5a8db8fc79928c033cacb703ce10f25a'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '62ca9db1-d39b-4fa0-827a-2d194f537712'
+};
